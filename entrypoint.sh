b1501b2bc72e821d4a1d5bcf414c9e064f74042a
Sebastian Schkudlara <sebastian.schkudlara@gmail.com>
2016-06-30 21:16:41 +0200

    Bundle install without dev and test

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,27 +5,26 @@ case "$1" in
     develop)
         echo "Running Development Server"
         bundle exec rake dir:exists RAILS_ENV=development
-        rm -f tmp/pids/server.pid
-        exec bundle exec rails s -p 3030 -b '0.0.0.0'
+
+        export SECRET_KEY_BASE=$(rake secret)
+
+        exec ./server start develop
         ;;
     test)
         echo "Running Test"
         bundle exec rake dir:exists RAILS_ENV=test
-        rm -f tmp/pids/server.pid
-        exec bundle exec rspec
-        ;;
-    staging)
-        echo "Running Start"
-        bundle exec rake dir:exists RAILS_ENV=staging
-        rm -f tmp/pids/server.pid
-        exec bundle exec puma -e staging -C config/puma.rb
+
+        export SECRET_KEY_BASE=$(rake secret)
+
+        exec rspec
         ;;
     start)
         echo "Running Start"
         bundle exec rake dir:exists RAILS_ENV=production
-        rm -f tmp/pids/server.pid
+
         export SECRET_KEY_BASE=$(rake secret)
-        exec bundle exec puma -e ${RAILS_ENV} -C config/puma.rb
+
+        exec ./server start production
         ;;
     *)
         exec "$@"
