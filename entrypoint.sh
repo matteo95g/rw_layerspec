39667534db6e24c0d0e0554f988b0229ce502345
Sebastian Schkudlara <sebastian.schkudlara@gmail.com>
2016-06-30 21:42:29 +0200

    Changes for entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,27 +4,21 @@ set -e
 case "$1" in
     develop)
         echo "Running Development Server"
-        bundle exec rake dir:exists RAILS_ENV=development
-
-        export SECRET_KEY_BASE=$(rake secret)
-
-        exec ./server start develop
+        rm -f tmp/pids/server.pid
+        exec bundle exec puma -e development -C config/puma.rb
         ;;
     test)
         echo "Running Test"
-        bundle exec rake dir:exists RAILS_ENV=test
-
-        export SECRET_KEY_BASE=$(rake secret)
-
-        exec rspec
+        rm -f tmp/pids/server.pid
+        exec bundle exec rspec
+        ;;
+    staging)
+        echo "Running Start"
+        exec bundle exec puma -e staging -C config/puma.rb
         ;;
     start)
         echo "Running Start"
-        bundle exec rake dir:exists RAILS_ENV=production
-
-        export SECRET_KEY_BASE=$(rake secret)
-
-        exec ./server start production
+        exec bundle exec puma -e ${RAILS_ENV} -C config/puma.rb
         ;;
     *)
         exec "$@"
