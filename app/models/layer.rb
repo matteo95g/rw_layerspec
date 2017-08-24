3c2e89b99e6612c93a9a80c43b7ee8bb90dbc148
Enrique Cornejo <enrique@cornejo.me>
2017-08-24 22:26:30 +0200

    Added new controller function for index

--- app/models/layer.rb
+++ app/models/layer.rb
@@ -19,9 +19,7 @@ class Layer
     check_slug
     downcase_provider
   end
-
   
-
   validates :name, presence: true, on: :create
   validates :slug, presence: true, format: { with: /\A[^\s!#$%^&*()（）=+;:'"\[\]\{\}|\\\/<>?,]+\z/,
                                              allow_blank: true,
@@ -37,15 +35,17 @@ class Layer
   scope :filter_inactives,   -> { where(status: 3)               }
   scope :filter_published,   -> { where(published: true)         }
   scope :filter_unpublished, -> { where(published: false)        }
+  scope :filter_env_prod,    -> { where(env: "production")       }
+  scope :filter_env_pre,     -> { where(env: "preproduction")    }
   scope :filter_actives,     -> { filter_saved.filter_published  }
 
-  scope :filter_app,     ->(app)         { where(application: /.*#{app}.*/i) }
-  scope :filter_dataset, ->(dataset_id)  { where(dataset_id: dataset_id)     }
+  scope :filter_app,         ->(app)         { where(application: /.*#{app}.*/i) }
+  scope :filter_dataset,     ->(dataset_id)  { where(dataset_id: dataset_id)     }
 
-  scope :filter_apps,     ->(apps)        { where({ application: { "$in" => apps        } }) }
-  scope :filter_datasets, ->(dataset_ids) { where({ dataset_id:  { "$in" => dataset_ids } }) }
-  scope :filter_name,     ->(name)        { where(name: /\d*#{name}\d*/i)                     }
-
+  scope :filter_apps,        ->(apps)        { where({ application: { "$in" => apps        } }) }
+  scope :filter_datasets,    ->(dataset_ids) { where({ dataset_id:  { "$in" => dataset_ids } }) }
+  scope :filter_name,        ->(name)        { where(name: /\d*#{name}\d*/i)                     }
+  
   def app_txt
     application.is_a?(String) ? application.split(',') : application
   end
@@ -69,8 +69,11 @@ class Layer
     end
 
     def fetch_all(options)
+      puts "OPTIONS"
+      puts options.to_json
       status        = options['status'].downcase if options['status'].present?
       published     = options['published']       if options['published'].present?
+      env           = options['env']             if options['env'].present?
       layerspec_app = options['app'].downcase    if options['app'].present?
       dataset       = options['dataset']         if options['dataset'].present?
       find_by_name  = options['name']            if options['name'].present?
@@ -91,6 +94,7 @@ class Layer
       layerspecs = layerspecs.filter_published   if published.present? && published.include?('true')
       layerspecs = layerspecs.filter_unpublished if published.present? && published.include?('false')
 
+
       layerspecs = if layerspec_app.present? && !layerspec_app.include?('all')
                      layerspecs.filter_app(layerspec_app)
                    else
@@ -100,6 +104,46 @@ class Layer
       layerspecs = filter_name(find_by_name) if find_by_name.present?
       layerspecs
     end
+
+    def fetch_all_index(options)
+      puts "OPTIONS"
+      puts options.to_json
+      status        = options['status'].downcase if options['status'].present?
+      published     = options['published']       if options['published'].present?
+      env           = options['env']             if options['env'].present?
+      layerspec_app = options['app'].downcase    if options['app'].present?
+      dataset       = options['dataset']         if options['dataset'].present?
+      find_by_name  = options['name']            if options['name'].present?
+
+      layerspecs = all
+      layerspecs = layerspecs.filter_dataset(dataset) if dataset.present?
+
+      layerspecs = case status
+                   when 'pending'  then layerspecs.filter_pending
+                   when 'active'   then layerspecs.filter_actives
+                   when 'failed'   then layerspecs.filter_failed
+                   when 'disabled' then layerspecs.filter_inactives
+                   when 'all'      then layerspecs
+                   else
+                     published.present? ? layerspecs : layerspecs.filter_actives
+                   end
+      layerspecs = layerspecs.filter_env_prod    if not env.present?
+      layerspecs = layerspecs.filter_env_prod    if env.present? && env.include?('production')
+      layerspecs = layerspecs.filter_env_pre     if env.present? && env.include?('preproduction')
+      layerspecs = layerspecs.filter_published   if published.present? && published.include?('true')
+      layerspecs = layerspecs.filter_unpublished if published.present? && published.include?('false')
+
+
+      layerspecs = if layerspec_app.present? && !layerspec_app.include?('all')
+                     layerspecs.filter_app(layerspec_app)
+                   else
+                     layerspecs
+                   end
+
+      layerspecs = filter_name(find_by_name) if find_by_name.present?
+      layerspecs
+    end
+
 
     def fetch_by_datasets(options)
       ids  = options['ids']                 if options['ids'].present?
